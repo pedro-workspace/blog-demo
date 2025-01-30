2b0946717bb55cf683a7bd8beed56a3e13ca8b2e
Pedro Mariano de Souza <pedroworkspace@gmail.com>
2025-01-30 16:56:55 -0300

    adicionando ngFor

---
+++ src/app/datasample/data.sample.ts
@@ -0,0 +1,21 @@
+
+const lorem:String = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+const url:String = "/img/img.jpg"
+
+
+export const dataSample: Array<{titulo:String,resumo:String,
+    nomeAutor:String,bioAutor:String,infoAcademica:String, imgUrl:String}> = [
+    {titulo:"Título", resumo:lorem, nomeAutor:"Author", 
+        bioAutor:"Informações sobre", infoAcademica:"Se houver", imgUrl:url},
+    {titulo:"Título", resumo:lorem, nomeAutor:"Author", 
+        bioAutor:"Informações sobre", infoAcademica:"Se houver", imgUrl:url},
+    {titulo:"Título", resumo:lorem, nomeAutor:"Author", 
+        bioAutor:"Informações sobre", infoAcademica:"Se houver", imgUrl:url},
+    {titulo:"Título", resumo:lorem, nomeAutor:"Author", 
+        bioAutor:"Informações sobre", infoAcademica:"Se houver", imgUrl:url},
+    {titulo:"Título", resumo:lorem, nomeAutor:"Author", 
+        bioAutor:"Informações sobre", infoAcademica:"Se houver", imgUrl:url},
+    {titulo:"Título", resumo:lorem, nomeAutor:"Author", 
+        bioAutor:"Informações sobre", infoAcademica:"Se houver", imgUrl:url},
+]
+
--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {dataSample} from '../datasample/data.sample'
 
 @Component({
   selector: 'app-homepage',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 
 export default class HomepageComponent {
-
+  posts = dataSample
 }
