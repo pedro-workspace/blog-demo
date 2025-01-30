e27e8eca28852008e78d354f56680586127d06f6
Pedro Mariano de Souza <pedroworkspace@gmail.com>
2025-01-30 15:01:19 -0300

    add more elements and styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import HomepageComponent from './homepage/homepage.component'
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path:"", component:HomepageComponent}, 
+    {path:'about', component:AboutComponent}, {path:"register", component:RegisterComponent}];
