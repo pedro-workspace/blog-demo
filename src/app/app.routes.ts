c64f37667c99cfc30beec8ae3825683b1e729252
Pedro Mariano de Souza <pedroworkspace@gmail.com>
2025-01-31 16:04:08 -0300

    updating styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import HomepageComponent from './homepage/homepage.component'
 import { AboutComponent } from './about/about.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [{path:"", component:HomepageComponent}, 
-    {path:'about', component:AboutComponent}, {path:"register", component:RegisterComponent}];
+    {path:'about', component:AboutComponent}, {path:"register", component:RegisterComponent},
+    {path:'login', component:LoginComponent}    
+];
---
+++ src/app/datasample/login.sample.ts
@@ -0,0 +1,8 @@
+
+
+
+export const loginList: Array<{login:String, password:String}> =  [
+
+    {login:"user", password:"12345"}
+
+]
